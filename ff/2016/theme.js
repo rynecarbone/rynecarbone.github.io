d16096f346a92c51ac5c16e0e455849e65fd0815
rynecarbone <karo@Karoliinas-MacBook-Air.local>
2016-10-17 18:13:31 +0200

    colors
    colors

--- ff/2016/theme.js
+++ ff/2016/theme.js
@@ -11,26 +11,21 @@ $(document).ready(
 	 }
 	 
 );
-$(document).ready(function(){
-	 	  var table = document.getElementById("power_table");
-			table.getElementsByTagName("tr").each(function(){
-				  var col_val = $(this).find("td:eq(9)").text();
-					window.alert(col_val);
-					if(col_val == "1"){
-						$(this).addClass('row-tier1');
-					}
-					else if(col_val == "2"){
-						$(this).addClass('row-tier2');
-					}
-					else if(col_val == "3"){
-						$(this).addClass('row-tier3');
-					}
-					else if(col_val == "4"){
-						$(this).addClass('row-tier4');
-					}
-					else if(col_val == "5"){
-						$(this).addClass('row-tier5');
-					}
-			});
-	 }
-);
+$(document).ready(function () {
+    $.each($("#power_table tr"), function (i, v) {
+        var txt = $(this).find("td:nth-child(10)").text();
+        document.getElementById('output').innerHTML = txt;
+        if (txt == "1") {
+            $(this).addClass("row-tier1");
+        }else if(txt == "2"){
+        	$(this).addClass("row-tier2");
+        }else if(txt == "3"){
+        	$(this).addClass("row-tier3");
+        }else if(txt == "4"){
+        	$(this).addClass("row-tier4");
+        }else if(txt == "5"){
+        	$(this).addClass("row-tier5");
+        }
+    });
+});
+
