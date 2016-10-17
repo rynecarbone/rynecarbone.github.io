07b0b8dcd59f5b1100925a76fc53215b0e0dca4c
rynecarbone <karo@Karoliinas-MacBook-Air.local>
2016-10-17 17:44:15 +0200

    t
    t

--- ff/2016/theme.js
+++ ff/2016/theme.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
 	 	  var table = document.getElementById("power_table");
 			table.getElementsByTagName("tr").each(function(){
 				  var col_val = $(this).find("td:eq(9)").text();
+					window.alert(col_val);
 					if(col_val == "1"){
 						$(this).addClass('row-tier1');
 					}
