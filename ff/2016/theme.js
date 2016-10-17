572869eb359b2f4e2399a7987883ea976189b854
rynecarbone <karo@Karoliinas-MacBook-Air.local>
2016-10-17 18:49:33 +0200

    d
    d

--- ff/2016/theme.js
+++ ff/2016/theme.js
@@ -13,17 +13,16 @@ $(document).ready(function () {
         }else if(txt == "5"){
         	$(this).addClass("row-tier5");
         }
-    });
-});
 
-$(document).ready(function () {
-    	$('#power_table').DataTable(
+    });
+		$('#power_table').DataTable(
 			    { "paging": false,
 				    "info"  : false,
 				     columnDefs:[
 					     { targets:[9],orderData:[9,0]}
 				     ]
 		    	}
-		  );
+		);
+
 });
 
