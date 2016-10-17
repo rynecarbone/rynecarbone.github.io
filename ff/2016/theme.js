2b975a284f39820654ae15e49de07c607073e433
rynecarbone <karo@Karoliinas-MacBook-Air.local>
2016-10-17 17:21:37 +0200

    row colors
    add row colors with some javascript

--- ff/2016/theme.js
+++ ff/2016/theme.js
@@ -1,10 +1,32 @@
-$(document).ready( function () {
-    $('#power_table').DataTable({
-				"paging": false,
-				"info"  : false,
-				columnDefs:[
-					{ targets:[9],orderData:[9,0]}
-				]
-
+$(document).ready( 
+		function () {
+    	$('#power_table').DataTable(
+			    { "paging": false,
+				    "info"  : false,
+				     columnDefs:[
+					     { targets:[9],orderData:[9,0]}
+				     ]
+		    	}
+		  );
+	 }
+	 function(){
+	 	  $("tr").each(function(){
+				  var col_val = $(this).find("td:eq(9)").text();
+					if(col_val == "1"){
+						$(this).addClass('row-tier1');
+					}
+					else if(col_val == "2"){
+						$(this).addClass('row-tier2');
+					}
+					else if(col_val == "3"){
+						$(this).addClass('row-tier3');
+					}
+					else if(col_val == "4"){
+						$(this).addClass('row-tier4');
+					}
+					else if(col_val == "5"){
+						$(this).addClass('row-tier5');
+					}
 			});
-} );
+	 }
+);
