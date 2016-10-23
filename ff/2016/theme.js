55013911e411e06857ed4fcf0c6d1c9fd76bf88e
rynecarbone <karo@Karoliinas-MacBook-Air.local>
2016-10-23 13:37:37 +0200

    adfa
    arras

--- ff/2016/theme.js
+++ ff/2016/theme.js
@@ -15,14 +15,5 @@ $(document).ready(function () {
 						}
 		    	}
 		);
-	 jQuery('[data-toggle="tooltip"]').each(function() {
-        var $elem = jQuery($(this));
-        $elem.tooltip({
-            html:true,
-            container: $elem,
-            delay: {hide:400}
-        });
-    }); 
-
 });
 
