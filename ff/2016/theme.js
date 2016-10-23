7d0ed846cc22621a1d3f273ff197168193594d14
rynecarbone <karo@Karoliinas-MacBook-Air.local>
2016-10-23 13:28:00 +0200

    slight update
    slight update, working on about page

--- ff/2016/theme.js
+++ ff/2016/theme.js
@@ -15,6 +15,14 @@ $(document).ready(function () {
 						}
 		    	}
 		);
-		
+	 jQuery('[data-toggle="tooltip"]').each(function() {
+        var $elem = jQuery($(this));
+        $elem.tooltip({
+            html:true,
+            container: $elem,
+            delay: {hide:400}
+        });
+    }); 
+
 });
 
